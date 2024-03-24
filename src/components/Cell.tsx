0ea7b26848563e580b150e216b0a781b3383c97b
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-24 19:18:56 +0200

    changed cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,16 +1,18 @@
 import { useRef, useState } from 'react';
+import { CellType } from '../types';
 
 type CellProps = {
-  name?: string;
+  cell: CellType;
 };
 
 type DivInput = HTMLDivElement | null;
 
-export const Cell = ({ name }: CellProps) => {
+export const Cell = ({ cell }: CellProps) => {
   const cellRef = useRef<DivInput>(null);
   const [cursor, setCursor] = useState('cursorCell');
-  const [selected, setSelected] = useState('');
-  const content = 'abc';
+  const [selectedClass, setSelected] = useState('');
+  const {cellStyles, contentStyles, selected, address, content, contentPaddingLeft} = cell;
+  
 
   function enableEditing(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     setCursor('cursorText');
@@ -24,7 +26,7 @@ export const Cell = ({ name }: CellProps) => {
     const range = document.createRange();
     const selection = window.getSelection();
     if (selection && content && content.length !== 0) {
-      const start = cellRef.current!.offsetLeft;
+      const start = cellRef.current!.offsetLeft + contentPaddingLeft;
       const letterWidth = cellRef.current!.offsetWidth / content.length;
       let pos = Math.min(
         Math.ceil((e.clientX - start) / letterWidth),
@@ -52,17 +54,21 @@ export const Cell = ({ name }: CellProps) => {
 
   return (
     <div
+      id={address}
+      style={cellStyles}
       data-testid="cell"
+      
       onDoubleClick={(e) => enableEditing(e)}
       onClick={() => selectCell()}
       onBlur={disableEditing}
-      className={`cell ${cursor} ${selected}`}
+      className={`cell ${cursor} ${selectedClass}`}
     >
       <div
         ref={cellRef}
         data-testid="content"
         spellCheck="false"
         className="content"
+        style={contentStyles}
       >
         {content}
       </div>
